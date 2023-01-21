007dcfbd5ed64a5f12cb55e04bf2d55b46346739
Harrem <52822616+Harrem@users.noreply.github.com>
2023-01-21 16:05:18 +0300

    app color pallete constants

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFF6750A4);
+  static const onPrimaryContainer = Color(0xFFEADDFF);
+  static const secondary = Color(0xFF625B71);
+  static const secondaryContainer = Color(0xFFE8DEF8);
+  static const tertiary = Color(0xFF7D5260);
+  static const tertiaryContainer = Color(0xFFFFD8E4);
+  static const surface = Color(0xFFFFFBFE);
+  static const surfaceVariant = Color(0xFFE7E0EC);
+
+  static const backgroundColor = Color(0xff000000);
+  static const containerColor = Color(0xFF090F20);
+  static const surfaceColor = Color(0xFF282F40);
+  static const subtitleColor = Color(0xFF646E78);
+  static const fonColor = Color(0xFF191C25);
+
+  static const primaryDark = Color(0xFF0A539F);
+  static const onPrimaryContainerDark = Color(0xFFEADDFF);
+  static const secondaryDark = Color(0xFF625B71);
+  static const secondaryContainerDark = Color(0xFFE8DEF8);
+  static const tertiaryDark = Color(0xFF7D5260);
+  static const tertiaryContainerDark = Color(0xFFFFD8E4);
+  static const surfaceDark = Color(0xFFFFFBFE);
+  static const surfaceVariantDark = Color(0xFFE7E0EC);
+}
