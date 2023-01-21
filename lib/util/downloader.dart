0b3fed81cd087209d69542df4869a3b3b7d78917
Harrem <52822616+Harrem@users.noreply.github.com>
2023-01-21 16:06:03 +0300

    added useful functions to get video info and more

--- lib/util/downloader.dart
+++ lib/util/downloader.dart
@@ -28,26 +28,77 @@ class Downloader {
 
   Future<void> downloadVideo(VideoStreamInfo video,
       {String? videoTitle, String? channelTitle}) async {
+    final httpR = HttpClient().userAgent;
+    debugPrint(httpR);
+
     var file = await createFile(
         fileName: "${videoTitle ?? video.tag}",
         dirName: channelTitle ?? "Unknown");
+
     if (file == null) return Future.error("Process Canceled");
+
     final sink = file.openWrite();
+
     debugPrint("video: ${video.url}");
     debugPrint("requesting vido stream");
+
     var request = await HttpClient().getUrl(Uri.parse(video.url.toString()));
+
     debugPrint("downloading");
+
     var response = await request.close();
+
     var total = response.contentLength;
     var received = 0;
+
     await for (var data in response) {
       received += data.length;
       final percentage = (received / total * 100).toStringAsFixed(0);
       print('Download progress: $percentage%');
       sink.add(data);
     }
+
     await sink.flush();
     sink.close();
+
+    debugPrint("Video Downloaded Successfully");
+  }
+
+  Future<void> downloadAudio(AudioStreamInfo audio,
+      {String? videoTitle, String? channelTitle}) async {
+    final httpR = HttpClient().userAgent;
+    debugPrint(httpR);
+
+    var file = await createFile(
+        fileName: "${videoTitle ?? audio.tag}",
+        dirName: channelTitle ?? "Unknown");
+
+    if (file == null) return Future.error("Process Canceled");
+
+    final sink = file.openWrite();
+
+    debugPrint("video: ${audio.url}");
+    debugPrint("requesting vido stream");
+
+    var request = await HttpClient().getUrl(Uri.parse(audio.url.toString()));
+
+    debugPrint("downloading");
+
+    var response = await request.close();
+
+    var total = response.contentLength;
+    var received = 0;
+
+    await for (var data in response) {
+      received += data.length;
+      final percentage = (received / total * 100).toStringAsFixed(0);
+      print('Download progress: $percentage%');
+      sink.add(data);
+    }
+
+    await sink.flush();
+    sink.close();
+
     debugPrint("Video Downloaded Successfully");
   }
 
