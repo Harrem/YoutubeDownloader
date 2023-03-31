7fc778cc027af07032f40fadd20b721eb106ede9
Harrem <52822616+Harrem@users.noreply.github.com>
2023-03-31 14:26:39 +0300

    implemented downloading with  flutter downloader package

--- lib/util/downloader.dart
+++ lib/util/downloader.dart
@@ -1,14 +1,24 @@
 import 'dart:async';
-
+import 'dart:convert';
+import 'dart:isolate';
+import 'dart:ui';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:youtube_explode_dart/youtube_explode_dart.dart';
 import 'package:flutter/foundation.dart';
+import 'package:ytd/util/permissions.dart';
+import '../models/downloaded_files.dart';
+import 'package:flutter_downloader/flutter_downloader.dart' as dl;
 import 'file_ops.dart';
 import 'dart:io';
 
 class Downloader {
   var ytExplode = YoutubeExplode();
-  StreamController<double> _downloadProgress =
+  final StreamController<double> _downloadProgress =
       StreamController<double>.broadcast();
+  bool _isPaused = false;
+  double _pausedProgress = 0.0;
+  late String _pausedSavePath;
+
   Future<Video> getVideoInfo(String videoId) async {
     var video = await ytExplode.videos.get(videoId);
     return video;
@@ -28,56 +38,77 @@ class Downloader {
     return stream;
   }
 
-  Future<void> downloadVideo(VideoStreamInfo video,
-      {String? videoTitle, String? channelTitle}) async {
-    final httpR = HttpClient().userAgent;
-    debugPrint(httpR);
+  Future<String?> download(String url, String fileName, String saveDir) async {
+    await dl.FlutterDownloader.registerCallback(downloadCallback);
+    var taskId = await dl.FlutterDownloader.enqueue(
+        url: url,
+        fileName: fileName,
+        savedDir: saveDir,
+        showNotification: true);
+    return taskId;
+  }
 
-    var file = await createFile(
-        fileName: "${videoTitle ?? video.tag}",
-        dirName: channelTitle ?? "Unknown");
-
-    if (file == null) Stream.error("Process Canceled");
-
-    final sink = file!.openWrite();
-
-    debugPrint("video: ${video.url}");
-    debugPrint("requesting vido stream");
-
-    var request = await HttpClient().getUrl(Uri.parse(video.url.toString()));
-
-    debugPrint("downloading");
-
-    var response = await request.close();
-
-    var total = response.contentLength;
-    var received = 0;
-    var percentage = 0.0;
-    response.listen(
-      (data) {
-        received += data.length;
-        sink.add(data);
-        final percentage = ((received / total) * 100);
-        _downloadProgress.add(percentage);
-        // final percentageText = ((received / total) * 100).toStringAsFixed(0);
-        // debugPrint('Download progress: $percentageText%');
-      },
-      onDone: () async {
-        debugPrint("Video Downloaded Successfully");
-        _downloadProgress.close();
-        await sink.flush();
-        sink.close();
-      },
-    );
-
-    // await for (var data in response) {
-    //   received += data.length;
-    //   final percentage = ((received / total) * 100).toStringAsFixed(0);
-    //   debugPrint('Download progress: $percentage%');
-    //   percentage;
-    //   sink.add(data);
-    //   return percentage;
+  static void downloadCallback(
+      String id, dl.DownloadTaskStatus status, int progress) {
+    // if (debug) {
+    print(
+        'Background Isolate Callback: task ($id) is in status ($status) and process ($progress)');
     // }
+    final SendPort send =
+        IsolateNameServer.lookupPortByName('downloader_send_port')!;
+    send.send([id, status, progress]);
+  }
+
+  Future<String?> downloadVideo(VideoStreamInfo video,
+      {required String videoTitle, String? channelTitle}) async {
+    var dir = await createDirectory(
+        channelTitle ?? 'Unknown', '/storage/emulated/0/ytd/');
+    var taskId = download(video.url.toString(), videoTitle, dir.path);
+    return taskId;
+    // final httpR = HttpClient().userAgent;
+    // debugPrint(httpR);
+
+    // var file = await createFile(
+    //     fileName: videoTitle, dirName: channelTitle ?? "Unknown");
+
+    // if (file == null) Stream.error("Process Canceled");
+
+    // final sink = file!.openWrite();
+
+    // debugPrint("video: ${video.url}");
+    // debugPrint("requesting vido stream");
+
+    // var request = await HttpClient().getUrl(Uri.parse(video.url.toString()));
+
+    // debugPrint("downloading");
+
+    // var response = await request.close();
+
+    // var length = response.contentLength;
+    // var received = 0;
+    // var progress = 0.0;
+    // response.listen(
+    //   (data) async {
+    //     if (_isPaused) {
+    //       _pausedProgress = progress / length;
+    //       await _saveDownloadStatus(
+    //           videoTitle, _pausedSavePath, false, _pausedProgress);
+    //       return;
+    //     }
+    //     received += data.length;
+    //     sink.add(data);
+    //     progress = (received / length);
+    //     _downloadProgress.add(progress);
+    //     await _saveDownloadStatus(videoTitle, file.path, true, progress);
+    //   },
+    //   onDone: () async {
+    //     debugPrint("Video Downloaded Successfully");
+    //     _downloadProgress.close();
+    //     await sink.flush();
+    //     sink.close();
+    //     await _saveDownloadStatus(videoTitle, file.path, true, 1.0);
+    //   },
+    // );
   }
 
   Stream<double> get progressStream => _downloadProgress.stream;
@@ -130,4 +161,35 @@ class Downloader {
     await file1RandomAccess.writeFrom(file2Content);
     await file1RandomAccess.close();
   }
+
+  Future<void> pauseDownload(String taskId) async {
+    await dl.FlutterDownloader.pause(taskId: taskId);
+  }
+
+  Future<void> resumeDownload(String taskId) async {
+    await dl.FlutterDownloader.resume(taskId: taskId);
+  }
+
+  // Future<void> _saveDownloadStatus(
+  //     String title, String savePath, bool isDownloaded, double progress) async {
+  //   final prefs = await SharedPreferences.getInstance();
+  //   final metadataJson = prefs.getString(savePath);
+  //   if (metadataJson != null) {
+  //     final metadata = DownloadedFile.fromJson(json.decode(metadataJson));
+  //     metadata.isDownloaded = isDownloaded;
+  //     metadata.progress = progress;
+  //     final newMetadataJson = json.encode(metadata.toJson());
+  //     await prefs.setString(savePath, newMetadataJson);
+  //   } else {
+  //     final metadata = DownloadedFile(
+  //       url: '',
+  //       title: title,
+  //       savePath: savePath,
+  //       isDownloaded: isDownloaded,
+  //       progress: progress,
+  //     );
+  //     final metadataJson = json.encode(metadata.toJson());
+  //     await prefs.setString(savePath, metadataJson);
+  //   }
+  // }
 }
