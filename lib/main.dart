d0ec80507f37989bfc9d90234a805af8c15b5dff
Harrem <52822616+Harrem@users.noreply.github.com>
2023-03-31 14:24:52 +0300

    initialized flutter downloader in main method

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:ytd/route.dart';
 import 'package:ytd/theme/custom_theme.dart';
+import 'package:flutter_downloader/flutter_downloader.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await FlutterDownloader.initialize(
+      debug:
+          true, // optional: set to false to disable printing logs to console (default: true)
+      ignoreSsl:
+          true // option: set to false to disable working with http links (default: false)
+      );
+
   runApp(const MyApp());
 }
 
